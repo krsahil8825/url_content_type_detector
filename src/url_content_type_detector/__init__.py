c6e5a55f9a62a69854d65c412f708f6a86c5a403
Kumar Sahil <krsahil8825@gmail.com>
2026-01-07 20:46:32 +0530

    Add GitHub Actions workflow for publishing to PyPI and update project metadata

--- src/url_content_type_detector/__init__.py
+++ src/url_content_type_detector/__init__.py
@@ -2,7 +2,7 @@
 URL Content Type Detector
 ~~~~~~~~~~~~~~~~~~~~~~~~~
 
-A utility to determine the content type of a URL. 
+A utility to determine the content type of a URL.
 
 Basic usage:
 
@@ -12,7 +12,7 @@ Basic usage:
     >>> print(content_type)
     image/png
 
-This module provides a function to fetch the content type of a given URL 
+This module provides a function to fetch the content type of a given URL
 by making an HTTP HEAD request.
 It also defines a custom exception for handling URL-related errors.
 
@@ -20,7 +20,11 @@ Copyright (c) 2026 Kumar Sahil
 Licensed under the MIT License.
 """
 
+from importlib.metadata import version
 from .detector import get_content_type
 from .exceptions import URLUtilsError
 
 __all__ = ["get_content_type", "URLUtilsError"]
+
+
+__version__ = version("url-content-type-detector")
