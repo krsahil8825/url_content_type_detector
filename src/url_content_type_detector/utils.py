3bde119baeb808add026282b2b2afc09e73ace16
Kumar Sahil <krsahil8825@gmail.com>
2026-01-28 19:32:56 +0530

    Add more utility functions to check additional content types for URLs

--- src/url_content_type_detector/utils.py
+++ src/url_content_type_detector/utils.py
@@ -8,9 +8,23 @@ This module provides utility functions to check the content type of a URL.
 from .detector import get_content_type
 
 
+def is_7z(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a 7z file."""
+    if "7z" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
 def is_audio(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is audio."""
     if "audio" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_avif(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an AVIF image."""
+    if "avif" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
         return True
     return False
 
@@ -82,6 +96,13 @@ def is_javascript(url: str, timeout: int | None = 10, is_secure: bool = True) ->
     return False
 
 
+def is_jpeg(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a JPEG image."""
+    if "jpeg" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
 def is_json(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is JSON."""
     if "json" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
@@ -95,10 +116,32 @@ def is_markdown(url: str, timeout: int | None = 10, is_secure: bool = True) -> b
         return True
     return False
 
+def is_mp3(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an MP3 audio file."""
+    if "mpeg" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_mp4(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an MP4 video file."""
+    if "mp4" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
 
 def is_octet_stream(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is an octet-stream."""
     if "octet-stream" in get_content_type(
+        url=url, timeout=timeout, is_secure=is_secure
+    ):
+        return True
+    return False
+
+
+def is_opendocument(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an OpenDocument file."""
+    if "opendocument" in get_content_type(
         url=url, timeout=timeout, is_secure=is_secure
     ):
         return True
@@ -112,9 +155,34 @@ def is_pdf(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     return False
 
 
+def is_png(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a PNG image."""
+    if "png" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_powerpoint(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a PowerPoint file."""
+    if "powerpoint" in get_content_type(
+        url=url, timeout=timeout, is_secure=is_secure
+    ) or "presentationml" in get_content_type(
+        url=url, timeout=timeout, is_secure=is_secure
+    ):
+        return True
+    return False
+
+
 def is_rar(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is a RAR file."""
     if "rar" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_svg(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an SVG image."""
+    if "svg" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
         return True
     return False
 
@@ -133,6 +201,29 @@ def is_video(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool
     return False
 
 
+def is_webp(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a WebP image."""
+    if "webp" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_word(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a Word document."""
+    if "word" in get_content_type(
+        url=url, timeout=timeout, is_secure=is_secure
+    ) or "msword" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_x_bz2(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a bzip2 file."""
+    if "x-bzip2" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
 def is_xml(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is XML."""
     if "xml" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
@@ -140,8 +231,29 @@ def is_xml(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     return False
 
 
+def is_x_tar(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a tar archive."""
+    if "x-tar" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_xz(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is an xz compressed file."""
+    if "xz" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
 def is_zip(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
     """Check if the content type of the URL is a ZIP file."""
     if "zip" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
+        return True
+    return False
+
+
+def is_zstd(url: str, timeout: int | None = 10, is_secure: bool = True) -> bool:
+    """Check if the content type of the URL is a zstd compressed file."""
+    if "zstd" in get_content_type(url=url, timeout=timeout, is_secure=is_secure):
         return True
     return False
